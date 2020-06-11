9213f09c8afd0ba1aedc6f51e3e0e85f6bf9da5e
Dean van Dugteren <hello@dean.press>
2020-06-11 02:38:04 +0200

    fix(notifications_handler): remove undeclared BRAVE_ADD_BRAVE_ADS_NOTIFICATION_HANDLER

--- chromium_src/chrome/browser/notifications/notification_display_service_impl.cc
+++ chromium_src/chrome/browser/notifications/notification_display_service_impl.cc
@@ -3,11 +3,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "brave/components/brave_ads/browser/ads_notification_handler.h"
+// #include "brave/components/brave_ads/browser/ads_notification_handler.h"
 
 // #define BRAVE_ADD_BRAVE_ADS_NOTIFICATION_HANDLER \
 //   AddNotificationHandler(                        \
 //       NotificationHandler::Type::BRAVE_ADS,      \
 //       std::make_unique<brave_ads::AdsNotificationHandler>(profile));
 #include "../../../../../../chrome/browser/notifications/notification_display_service_impl.cc"
-#undef BRAVE_ADD_BRAVE_ADS_NOTIFICATION_HANDLER
+// #undef BRAVE_ADD_BRAVE_ADS_NOTIFICATION_HANDLER
