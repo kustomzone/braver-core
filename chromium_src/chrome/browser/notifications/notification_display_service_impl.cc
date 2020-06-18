2092cdfff6229d2602036a8f87229167abdd92d3
Dean van Dugteren <hello@dean.press>
2020-06-18 19:48:31 +0200

    chore: remove BRAVE_ADD_BRAVE_ADS_NOTIFICATION_HANDLER

--- chromium_src/chrome/browser/notifications/notification_display_service_impl.cc
+++ chromium_src/chrome/browser/notifications/notification_display_service_impl.cc
@@ -4,16 +4,10 @@
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "brave/components/brave_ads/browser/ads_notification_handler.h"
-#include "brave/components/brave_rewards/browser/buildflags/buildflags.h"
 
-#if BUILDFLAG(BRAVE_REWARDS_ENABLED)
- #define BRAVE_ADD_BRAVE_ADS_NOTIFICATION_HANDLER \
-  AddNotificationHandler(                        \
-       NotificationHandler::Type::BRAVE_ADS,      \
-       std::make_unique<brave_ads::AdsNotificationHandler>(profile));
-#else
 #define BRAVE_ADD_BRAVE_ADS_NOTIFICATION_HANDLER \
-  (void)0
-#endif
+  // AddNotificationHandler(                        \
+  //     NotificationHandler::Type::BRAVE_ADS,      \
+  //     std::make_unique<brave_ads::AdsNotificationHandler>(profile));
 #include "../../../../../../chrome/browser/notifications/notification_display_service_impl.cc"
 #undef BRAVE_ADD_BRAVE_ADS_NOTIFICATION_HANDLER
